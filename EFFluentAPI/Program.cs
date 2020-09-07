19c0e1ac2d412931b1520811eefce4645c024939
Mohd Naved Alam <mohdnavedalam87@gmail.com>
2020-09-07 22:51:12 +0530

    Section 6 - Lecture 47 - LINQ in Action

--- EFFluentAPI/Program.cs
+++ EFFluentAPI/Program.cs
@@ -3,12 +3,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using EFFluentAPI.Model;
+
 namespace EFFluentAPI
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var Context = new FluentAPIContext();
+
+            //LINQ Syntax
+            var query = 
+                from c in Context.Courses
+                where c.Name.Contains("C#") //Remove this line to display all courses
+                orderby c.Name select c;
+
+            foreach(var c in query)
+                Console.WriteLine(c.Name);
+
+            //Extension Methods
+            var courses = Context.Courses
+                .Where(c => c.Name.Contains("C#"))
+                .OrderBy(c => c.Name);
+
+            foreach(var d in courses)
+                Console.WriteLine(d.Name);
         }
     }
 }
