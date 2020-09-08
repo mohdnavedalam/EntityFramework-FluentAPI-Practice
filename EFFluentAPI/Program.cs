400ac48dc9c57c7deefcd4fc1f5b6654867da9da
Mohd Naved Alam <mohdnavedalam87@gmail.com>
2020-09-08 18:03:56 +0530

    New Commit

--- EFFluentAPI/Program.cs
+++ EFFluentAPI/Program.cs
@@ -17,18 +17,33 @@ namespace EFFluentAPI
             var query = 
                 from c in Context.Courses
                 where c.Name.Contains("C#") //Remove this line to display all courses
-                orderby c.Name select c;
+                orderby c.Name
+                select c;
 
             foreach(var c in query)
                 Console.WriteLine(c.Name);
 
-            //Extension Methods
+            Console.WriteLine("\n\n\n");
+
+            //Extension Methods            
             var courses = Context.Courses
                 .Where(c => c.Name.Contains("C#"))
                 .OrderBy(c => c.Name);
 
             foreach(var d in courses)
                 Console.WriteLine(d.Name);
+
+            Console.WriteLine("\n\n\n");
+
+            var query2 =
+                from c in Context.Courses
+                where c.Level == CourseLevel.Beginner && c.Author.ID == 1
+                select c;
+
+            foreach(var c in query2)
+                Console.WriteLine(c.Name);
+
+            Console.ReadLine();
         }
     }
 }
